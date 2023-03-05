f606612bf76eb4863ef94089e8922712d980e15f
seyyid <seyyidmcelik@gmail.com>
2023-03-05 14:22:45 +0300

    tag page full screen photo bug fix

--- public/scripts/tag.js
+++ public/scripts/tag.js
@@ -7,3 +7,22 @@ tagContainer.addEventListener('click', () => {
     arrow.classList.toggle('active-arrow')
 })
 /* Tags Controle for Mobile End */
+
+/* Profile Photo Full Screen Start */
+let modal = document.getElementById('photo-modal')
+let closeBtn = document.getElementById('close')
+let fullScreens = document.querySelectorAll('.getfullScreen')
+let photoModalImg = document.getElementById('photoModalImg')
+fullScreens.forEach(function (fullScreen) {
+    fullScreen.addEventListener('click', () => {
+        photoModalImg.setAttribute('src', fullScreen.getAttribute('data-url'))
+        photoModalImg.setAttribute('alt', fullScreen.getAttribute('data-name'))
+        modal.classList.add('active-modal')
+        document.body.style.overflowY = 'hidden'
+    })
+})
+closeBtn.addEventListener('click', () => {
+    modal.classList.remove('active-modal')
+    document.body.style.overflowY = 'auto'
+})
+/* Profile Photo Full Screen End */
